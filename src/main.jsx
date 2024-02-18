d6b76129e1e9cdf30c909521fbdc252cc083a5dc
Ian <goldfeld35@gmail.com>
2024-02-18 19:26:50 -0300

    style: adding a Theme

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import ReactDOM from "react-dom/client"
 import { App } from "./App.jsx"
 
 import { GlobalStyle } from "./styles/global.js"
+import { theme } from "./styles/theme.js";
+import { ThemeProvider } from "styled-components"; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
